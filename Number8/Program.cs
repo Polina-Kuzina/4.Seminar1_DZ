25d23adc98fec08567abf358aac93636a1eb3e74
Polina <payline@mail.ru>
2022-02-02 20:34:54 +0300

    Решена 4 задача

---
+++ Number8/Program.cs
@@ -0,0 +1,13 @@
+﻿// 8. Показать четные числа от 1 до N
+Console.Write("Задайте значение N: ");
+int N = int.Parse(Console.ReadLine());
+int num = 1;
+while(num <= N)
+{
+    int b = num % 2;
+    if(b == 0)
+{
+    Console.WriteLine(num);
+}
+    num++;
+}
