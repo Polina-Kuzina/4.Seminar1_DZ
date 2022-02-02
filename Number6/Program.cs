f58fbbda571bec2b3009f8302333a3643cf51f6d
Polina <payline@mail.ru>
2022-02-02 20:18:37 +0300

    3 задача решена

---
+++ Number6/Program.cs
@@ -0,0 +1,13 @@
+﻿// 6. Выяснить является ли число чётным
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = a % 2;
+if(b == 0)
+{
+    Console.WriteLine("Это число четное");
+}
+else
+{
+    Console.WriteLine("Это число нечетное");
+}
+
